e47755e3bb972a9b93c2391aa1dd5e50312d7e20
isabellehuangg <huang.yuqing.issy@gmail.com>
2023-09-23 00:52:15 -0400

    Homepage

---
+++ client/src/Components/Constants/Links.js
@@ -0,0 +1,16 @@
+const beforeLoginLinks = [
+    {
+        
+    }
+];
+
+const afterLoginLinks = [
+    {
+        text: 'About',
+        url: '/'
+    },
+    {
+        text: 'Search Classes',
+        url: '/search'
+    }
+];
--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,14 +1,15 @@
 const Home = () => {
-    return ( 
-        <div className="Home">
-            <h1>Unlock Your Academic Journey:</h1>
-            <p>Join, Learn, and Collaborate!</p>
-            <div className="buttons">
-                <button className="button">Log In</button>
-                <button className="button">Sign Up</button>
-            </div>
+    return (
+      <div className="Home">
+        <h1 className="title">Unlock Your Academic Journey</h1>
+        <p className="description">Join, Learn, and Collaborate!</p>
+        <div className="buttons">
+          <button className="button" style={{ background: 'linear-gradient(190deg, #f4ad63, #fadca7)' }}>Log In</button>
+          <button className="button">Sign Up</button>
         </div>
-     );
-}
- 
-export default Home;
+      </div>
+    );
+  }
+  
+  export default Home;
+  
