816173ceb93c778339443de7ef753b3fee6731fb
isabellehuangg <huang.yuqing.issy@gmail.com>
2023-09-23 15:03:22 -0400

    Form

--- client/src/Components/ClassSearch.js
+++ client/src/Components/ClassSearch.js
@@ -1,7 +1,27 @@
+import { classes } from "./Constants/Class";
+import { useState } from "react";
+
 const ClassSearch = () => {
+    // fetch possible classes from db, for now, set as classes I have
     return ( 
         <div className="ClassSearch">
-            <h1>Hello</h1>
+            <h1>Find Your Classes</h1>
+            <form>
+            {classes.map((classItem) => (
+                <div key={classItem.id}>
+                <label htmlFor={classItem.id} className="selectClass">{classItem.label}</label>
+                    <select id="classes" className="select-box">
+                        <option disabled selected value> {classItem.label} </option>
+                        {classes.map((classCode) => (
+                            <option key={classCode.value} value={classCode.value}>
+                                {classCode.label}
+                            </option>
+                        ))}
+                    </select>
+                </div>
+            ))}
+            </form>
+            <button className="button">Enroll</button>
         </div>
      );
 }
---
+++ client/src/Components/Constants/Class.js
@@ -0,0 +1,8 @@
+const classes = [
+    { id: 'Class 1', label: 'Class 1 *', type: 'class1' },
+    { id: 'Class 2', label: 'Class 2', type: 'class2' },
+    { id: 'Class 3', label: 'Class 3', type: 'class3' },
+    { id: 'Class 4', label: 'Class 4', type: 'class4' },
+];
+
+export { classes };
