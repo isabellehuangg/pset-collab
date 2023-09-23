e863a9786c3ee0125c0b1144d96b8674dd99fc1d
isabellehuangg <huang.yuqing.issy@gmail.com>
2023-09-22 23:03:35 -0400

    Initial frontend template

---
+++ client/src/Components/Home.js
@@ -0,0 +1,10 @@
+const Home = () => {
+    return ( 
+        <div className="Home">
+            <h1>Pset Collab</h1>
+            <p>A place to collaborate on problem sets</p>
+        </div>
+     );
+}
+ 
+export default Home;
